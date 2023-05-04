25ed73cd230a90f62c7a1a772d9ccec36db36e6c
daotrongthuan1010 <conxinloime1010@gmail.com>
2023-05-04 21:22:20 +0700

    update class search

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,18 +1,50 @@
 import React from "react";
 
 class Search extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+        strSearch : ''
+    };
+    this.handlerChange = this.handlerChange.bind(this);
+    this.handlerSearch = this.handlerSearch.bind(this);
+    this.handlerClear = this.handlerClear.bind(this);
+  }
+  handlerSearch(){
+    console.log(this.state.strSearch);
+  }
+  handlerChange(event){
+    this.setState({
+      strSearch: event.target.value
+    })
+    
+  }
+  handlerClear(){
+    this.setState(
+      {
+        strSearch : ''
+      }
+    );
+  }
+
   render() {
     return (
       <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
         <div className="input-group">
           <input
+            value={this.state.strSearch}
+            onChange = {this.handlerChange}
             type="text"
             className="form-control"
             placeholder="Nhập tìm kiếm..."
           />
           <span className="input-group-btn">
-            <button className="btn btn-info" type="button">
+            <button onClick = {this.handlerSearch} className="btn btn-info" type="button">
              Tìm kiếm
+            </button>
+            <button onClick = {this.handlerClear} className="btn btn-info" type="button">
+             Xóa
             </button>
           </span>
         </div>
