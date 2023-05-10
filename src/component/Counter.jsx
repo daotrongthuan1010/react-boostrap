d2c76f5531d2df4aedb77ba64ee1ffab6bddac98
daotrongthuan1010 <conxinloime1010@gmail.com>
2023-05-10 21:18:41 +0700

    update react hook

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,27 +1,13 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useState } from 'react';
 
-function Counter(props) {
+function Counter() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      <h1>Count: {props.count}</h1>
-      <button onClick={props.increment}>Increment</button>
-      <button onClick={props.decrement}>Decrement</button>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
-
-function mapStateToProps(state) {
-  return {
-    count: state.count,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    increment: () => dispatch({ type: "INCREMENT" }),
-    decrement: () => dispatch({ type: "DECREMENT" }),
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
---
+++ src/component/User.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://example.com/api/users')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {users.map(user => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
+}
+
+export default Users;
